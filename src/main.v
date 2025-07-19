c29b5407e502e12451a75f80978ff4979bbef0d7
⚡ Sigui 🇨🇮 <siguici@proton.me>
2025-07-19 10:46:57 +0100

    ✨ Add a runtime

--- src/main.v
+++ src/main.v
@@ -1,13 +1,47 @@
+module main
+
 import os
+import cli
+import term
 
-fn main() {
-	path := 'test/samples/hello.cup'
-	source := os.read_file(path) or { panic(err) }
-	lines := tokenize(source)
-	nodes := parse(lines)
-	html := render(nodes)
+pub fn main() {
+	mut app := cli.Command{
+		name:        'cup'
+		description: 'Run Cup scripts or codes'
+		usage:       '[file_or_code]'
+		execute:     fn (cmd cli.Command) ! {
+			args := cmd.args
 
-	println(path)
+			if args.len < 1 {
+				mut code := ''
+				q := term.bg_blue(term.white(term.bold(' \\q ')))
+				exit_i := term.bg_magenta(term.white(term.bold(' exit() ')))
+				ctrl_c := term.bg_black(term.white('Ctrl-C'))
+				println('Welcome to the experimental ${term.cyan('Cup')} REPL')
+				println('Type ${q} or ${exit_i} or press ${ctrl_c} to exit:')
+				for {
+					if mut line := os.input_opt('❯ ') {
+						if line !in ['\\q', 'exit()'] {
+							code += line + '\n'
+							continue
+						}
+						break
+					}
+				}
+				if code == '' {
+					h := cmd.help_message()
+					println(h)
+					exit(0)
+				}
+				run_code(code: code)
+			}
 
-	println(html)
+			for arg in cmd.args {
+				run(arg)
+			}
+		}
+	}
+
+	app.setup()
+	app.parse(os.args)
 }
---
+++ src/runtime.v
@@ -0,0 +1,48 @@
+module main
+
+import os
+
+@[params]
+struct RunOptions {
+	code string
+	file string
+}
+
+pub fn run(code string) {
+	mut path := code
+	if os.exists(path) {
+		run_path(path)
+	} else {
+		if !path.ends_with('.cup') {
+			path += '.cup'
+		}
+
+		if os.is_file(path) {
+			run_file(path)
+		} else {
+			run_code(code: code)
+		}
+	}
+}
+
+pub fn run_path(path string) {
+	if os.is_dir(path) {
+		for file in os.ls(path) or { panic(err) } {
+			run_file(file)
+		}
+	} else if os.is_file(path) {
+		run_file(path)
+	}
+}
+
+pub fn run_file(file string) {
+	code := os.read_file(file) or { panic(err) }
+	run_code(code: code, file: file)
+}
+
+pub fn run_code(opts RunOptions) {
+	lines := tokenize(opts.code)
+	nodes := parse(lines)
+	html := render(nodes)
+	dump(html)
+}
